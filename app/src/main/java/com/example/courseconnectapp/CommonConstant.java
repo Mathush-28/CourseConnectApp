12ad2784a7dea971b7d7b00dcef2d8aaaca6af8b
Dajev007 <dajevbass@gmail.com>
2024-07-12 12:47:16 +0530

    final

---
+++ app/src/main/java/com/example/courseconnectapp/CommonConstant.java
@@ -0,0 +1,24 @@
+package com.example.courseconnectapp;
+
+public class CommonConstant {
+
+        public final static String EMAIL ="dajevbass@gmail.com";
+        public final static String PASSWORD = "dpjrydoxjipznmlp";
+
+        public final static String SIGNING_USER = "valid_user";
+
+        public final static String GUEST_USER= "guest_user";
+
+        public final static String ADMIN_USER= "admin_user";
+
+        public final static String ADMIN_EMAIL ="admin";
+
+        public final static String ADMIN_PASSWORD= "admin";
+
+
+    }
+
+
+
+
+
